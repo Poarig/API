0f8f2538939a72a05e4a193c4ab0fe2410f5adad
Podvolotskiy Arkadiy <Poarig@yandex.ru>
2021-02-10 18:33:05 +0300

    3

--- 1.py
+++ 1.py
@@ -10,7 +10,7 @@ class Paramests(object):
         self.ll = "37.530887,55.703118"
         self.z = 12
         self.l = "map"
-        self.size = "600,450"
+        self.size = "650,450"
 
 
 def load_map(p):
@@ -26,6 +26,24 @@ def load_map(p):
     return response
 
 
+def move(x, y, size):
+    key = pygame.key.get_pressed()
+    if key[pygame.K_UP]:
+        if y + 10 <= 0:
+            y += 10
+    if key[pygame.K_DOWN]:
+        if y + size[1] - 10 >= 450:
+            y -= 10
+    if key[pygame.K_LEFT]:
+        if x + 10 <= 0:
+            x += 10
+    if key[pygame.K_RIGHT]:
+        if x + size[0] - 10 >= 600:
+            x -= 10
+
+    return x, y
+
+
 p = Paramests()
 response = load_map(p)
 # Запишем полученное изображение в файл.
@@ -33,15 +51,27 @@ map_file = "map.png"
 with open(map_file, "wb") as file:
     file.write(response.content)
 
+image = pygame.image.load(map_file)
+
 # Инициализируем pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
 # Рисуем картинку, загружаемую из только что созданного файла.
-screen.blit(pygame.image.load(map_file), (0, 0))
+x, y = 0, 0
+image = pygame.image.load(map_file)
+screen.blit(image, (x, y))
 # Переключаем экран и ждем закрытия окна.
 pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        x, y = move(x, y, image.get_size())
+
+    screen.blit(image, (x, y))
+    pygame.display.flip()
+
 pygame.quit()
 
 # Удаляем за собой файл с изображением.
